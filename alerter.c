b153b650868dc76ef3f6334e3a34350e80e0411c
azharmj <46325751+azharmj@users.noreply.github.com>
2022-12-06 18:30:11 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,45 +1,84 @@
 #include <stdio.h>
 #include <assert.h>
-#define DEFAULT_STUB_VALUE 200
-#define DEFAULT_INTEGRATION_ENV 1
-#define DEFAULT_TEST_ENV 2
+
+
+#define PRODUCTION_ENV  1
+#define TESTCODE_ENV         2
 
 int alertFailureCount = 0;
-int Current_Env = DEFAULT_INTEGRATION_ENV;
+int Current_Env = PRODUCTION_ENV;
 
+/*Conversion from farenheit to celcius*/
+float convertfarenheittocelcius(float farenheit)
+{
+    float celcius = (farenheit - 32) * 5 / 9;
+    return celcius;
+}
 
 int networkAlertStub(float celcius) {
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    return 200;
-}
-
-void alertInCelcius(float farenheit) {
-    float celcius = (farenheit - 32) * 5 / 9;
-    int returnCode;
-    if(Current_Env == DEFAULT_INTEGRATION_ENV)
+    if(celcius<=200)
     {
-         returnCode = celcius;
-    }
+     return 200;
+     }
     else
     {
-         returnCode = networkAlertStub(celcius);
+     return 500;
     }
+}
+
+int networkAlertProduction(float celcius){
+  if(celcius<=200)
+  {
+     return 200;
+  }
+  else
+  {
+    return 500;
+  }
+}  
+
+
+/*Selection of the environment: Productio/Testing */
+float SelectAlertEnvironment(float celcius, int Current_Env)
+{
+   float calculated_Value;
+   if(Current_Env == PRODUCTION_ENV )
+   {
+	   /*Select Production environmnt*/
+     calculated_Value = networkAlertProduction(celcius);
+   }
+   else
+   {
+	   /*Select Test environmnt*/
+     calculated_Value = networkAlertStub(celcius);
+   }
+   return calculated_Value;
+}
+
+void alertInCelcius(float farenheit,int Current_Env) {
+	
+    float celcius  = convertfarenheittocelcius(farenheit);
+	
+    int returnCode = SelectAlertEnvironment(celcius,Current_Env);
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+        alertFailureCount += 1;
     }
 }
 
 int main() {
-    alertInCelcius(400.5);
-    alertInCelcius(303.6);
-    assert(alertFailureCount == 1); 
+    alertInCelcius(380.5,TESTCODE_ENV);
+    assert(alertFailureCount == 0);
+    alertInCelcius(303.6,PRODUCTION_ENV);
+    assert(alertFailureCount == 0);
+    alertInCelcius(515.5,PRODUCTION_ENV);
+    assert(alertFailureCount == 1);
+    alertInCelcius(515.5,TESTCODE_ENV);
+    assert(alertFailureCount == 2);
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
     return 0;
